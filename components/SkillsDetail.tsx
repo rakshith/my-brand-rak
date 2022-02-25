b6e58ee95bc7538e1b61627acb34d3abdc56e3b7
Rakshith <rakshith.raj@tigerspike.com>
2022-02-25 15:09:38 +0530

    alignment added

--- components/SkillsDetail.tsx
+++ components/SkillsDetail.tsx
@@ -4,11 +4,13 @@ import LineProgressBar from './LineProgressBar'
 function SkillsDetail() {
   return (
     <div className="flex flex-col">
-      <div className="skill-header py-4">Coding</div>
+      <div className="skill-header mt-4">Coding</div>
       <LineProgressBar value={90} title={'ReacJS/NextJS'} />
       <LineProgressBar value={80} title={'React Native Mobile'} />
       <LineProgressBar value={76} title={'HTML/CSS'} />
       <LineProgressBar value={70} title={'MicroService Architecture'} />
+      <LineProgressBar value={50} title={'NodeJS/NestJS'} />
+      <LineProgressBar value={30} title={'Java'} />
     </div>
   )
 }
